9919db882ea450e991f8b687b4761f9c2b5192f3
shuheydev <shuhey.dev@gmail.com>
2020-08-15 11:20:59 +0900

    implemented highlight-on-google search feature.
    it execute after google search automatically if the feature is toggled.

--- popup/popup.js
+++ popup/popup.js
@@ -5,8 +5,8 @@
         window.browser = chrome;
     }
 
+    //send message to contentScript for start highlight.
     function sendSearchMessage(tabs) {
-
         let targetTextArea = document.querySelector("#textAreaTarget");
         let targetString = targetTextArea.value;
 
@@ -14,20 +14,20 @@
 
         browser.tabs.sendMessage(tabs[0].id, { command: "search", target: targetString });
     }
-
     //Set Eventhandler to 'Search' button.
     let replaceButton = document.querySelector("#buttonSearch");
     replaceButton.addEventListener('click', (e) => {
+        console.log('search')
         browser.tabs.query({ active: true, currentWindow: true }, sendSearchMessage);
     });
-
-
+    //send message to contentScript for remove highlight
     function sendClearMessage(tabs) {
         browser.tabs.sendMessage(tabs[0].id, { command: "clear" });
     }
     //Set Eventhandler to 'Search' button.
     let clearButton = document.querySelector("#buttonClear");
     clearButton.addEventListener('click', (e) => {
+        console.log('clear')
         browser.tabs.query({ active: true, currentWindow: true }, sendClearMessage);
     });
 
@@ -38,11 +38,21 @@
         }
     });
 
+    let toggle = document.querySelector('#toggle_highlight_on_search');
+    toggle.addEventListener('click', (e) => {
+        chrome.storage.local.set({ 'IsHighlightOnSearchEnabled': toggle.checked }, () => {
+        });
+    });
+
     //Restore previous input words to textarea when popup shown.
     let targetTextArea = document.querySelector("#textAreaTarget");
     targetTextArea.value = localStorage.getItem('target');
     targetTextArea.focus();
 
+    chrome.storage.local.get("IsHighlightOnSearchEnabled", function (result) {
+        let isHighlightOnSearchEnabled = result.IsHighlightOnSearchEnabled;
+        toggle.checked = isHighlightOnSearchEnabled;
+    });
 })();
 
 
